6bd845c3fa39b29ef82df5d094c0c42d2a40e792
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2021-07-23 14:09:18 +0700

    add firebase config and get data

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -1,4 +1,5 @@
-import {ref} from 'vue'
+import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
 
 const getPosts = () => {
     const posts = ref([]);
@@ -6,18 +7,25 @@ const getPosts = () => {
 
     const load = async () => {
         try {
+            // get data from API
+            // let data = await fetch("http://localhost:3000/posts");
+            // if (!data.ok) {
+            //     throw Error("data is not available");
+            // }
+            // posts.value = await data.json();
 
-            let data = await fetch("http://localhost:3000/posts");
-            if (!data.ok) {
-                throw Error("data is not available");
-            }
-            posts.value = await data.json();
+            //get data from firebase
+            const res = await projectFirestore.collection('posts').get();
+            posts.value = res.docs.map(item => {
+                // console.log(doc.data())
+                return { ...item.data(), id: item.id }
+            })
         } catch (err) {
             error.value = err.message;
         }
     };
 
-    return {posts,error,load}
+    return { posts, error, load }
 }
 
-export default getPosts ;
+export default getPosts;
---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBt9cU269EOYMW4K_3q3T1q-mC-5qYO74c",
+    authDomain: "blog-vue-a2f67.firebaseapp.com",
+    databaseURL: "https://blog-vue-a2f67.firebaseio.com",
+    projectId: "blog-vue-a2f67",
+    storageBucket: "blog-vue-a2f67.appspot.com",
+    messagingSenderId: "790507236164",
+    appId: "1:790507236164:web:9ca6ce89c32ce4452e4511",
+    measurementId: "G-RGWHL4CRSY"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+
+export {
+    projectFirestore
+}
